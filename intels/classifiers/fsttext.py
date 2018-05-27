651cafb91a385aacc95e72a716ccbec35a8351b9
einstein <jubinjose92@gmail.com>
2018-05-27 19:03:37 +0530

    able to use pretrained vectors

--- intels/classifiers/fsttext.py
+++ intels/classifiers/fsttext.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import os
 import fasttext
 
 class Fsttext:
@@ -7,13 +8,19 @@ class Fsttext:
 	training_file = 'train.txt'
 	model_file = 'model'
 
-	def __init__(self, train_file_name, model_file_name):
+	def __init__(self, train_file_name, model_file_name, pretrained_vec_file_name, dim):
 		self.training_file = train_file_name
 		self.model_file = model_file_name
+		self.pretrained_vec_file_name = pretrained_vec_file_name
+		self.dim = dim
 
 	def train(self):
 		print('--- starting training ---')
-		fasttext.supervised(self.training_file, self.model_file)
+		if os.path.exists(self.pretrained_vec_file_name):
+			print('found pretrained word vector')
+			fasttext.supervised(self.training_file, self.model_file, pretrained_vectors=self.pretrained_vec_file_name, dim=self.dim)
+		else:
+			fasttext.supervised(self.training_file, self.model_file)
 		print('--- fininshed training ---')
 
 	def predict(self, text):
